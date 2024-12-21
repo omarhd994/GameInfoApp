6f7a8a664a0a5513c6cda84cdb53ecb417180120
omar <omarhd994@gmail.com>
2024-12-21 01:28:59 +0100

    Added: metacritic.js to fetch Data, Updated: App.js to handle data, Fixed: map(), Added: styles.card

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import favicon from './assets/favicon.png';
+import { useState, useEffect } from 'react';
+import { metacritic } from './lib/metacritic';
 
 export default function App() {
+  const [games, setGames] = useState([])
+
+  useEffect (() => {
+    
+    metacritic().then((games) => {
+      setGames(games)
+    })
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image fadeDuration={2} 
+      source={favicon} style={{
+        width: 100, 
+        height: 100,
+        }} />
+      <Text style={styles.fontColor}>WELCOME!!</Text>
+      <StatusBar style="light" />
+      <Button color='grey' title='Appuyez ici!' onPress={() => alert('Bonjour')} />
+      
+      {games.map((game) => (
+        <View key={game.id} style={styles.card}>
+          <Image 
+          source={{ uri: game.thumbnail }}
+          style={{
+            width: 107,
+            height: 147,
+            borderRadius: 10,
+          }}
+          />
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +45,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fontColor: {
+    color: '#fff'
+  },
+  card: {
+    margin: 5,
+    padding: 5,
+  },
 });
+  
