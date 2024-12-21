79e4c36051f5c870d368da25f5253b09e74c6d24
omar <omarhd994@gmail.com>
2024-12-21 14:28:26 +0100

    Added: svg logo of metacritic and adapted, useRef, Animated animation and opacity effect

--- component/GameCard.jsx
+++ component/GameCard.jsx
@@ -1,4 +1,5 @@
-import { View, Text, Image, StyleSheet } from "react-native"
+import { useEffect, useRef } from "react";
+import { View, Text, Image, StyleSheet, Animated } from "react-native"
 export const GameCard = ({ game }) => {
   return (
         <View key={game.id} style={styles.card}>
@@ -8,6 +9,25 @@ export const GameCard = ({ game }) => {
           <Text style={styles.platform}>{game.platform}</Text>
           <Text style={styles.releasedate}>{game.release_date}</Text>
         </View>  )
+}
+
+export function AnimatedGameCard({game, index}) {
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 1000,
+            delay: index * 250,
+            useNativeDriver: true,
+        }).start();
+    }, [ opacity, index ])
+
+    return (
+        <Animated.View style={{ opacity }}>
+            <GameCard game={game}/>
+        </Animated.View>
+    )
 }
 
 const styles = StyleSheet.create({
--- component/Main.jsx
+++ component/Main.jsx
@@ -1,28 +1,34 @@
-import { ScrollView, ActivityIndicator, View, FlatList } from 'react-native';
+import { ActivityIndicator, View, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
-import { metacritic } from '../lib/metacritic';
-import { GameCard } from './GameCard';
+import { getLatestGames } from '../lib/metacritic';
+import { AnimatedGameCard } from './GameCard';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Logo } from './Logo';
 
 export function Main() {
   const [games, setGames] = useState([])
   const insets = useSafeAreaInsets();
   useEffect (() => {
-    metacritic().then((games) => {
+    getLatestGames().then((games) => {
       setGames(games)
     })
   }, []);
 
+
   
   return (
 <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+    <View style={{ marginBottom: 20, alignItems: 'center',}}>
+      <Logo />
+    </View>
       {games.length === 0 ? (
           <ActivityIndicator color={'red'} size={'large'}/>
       ) : (
      <FlatList // This only renders what we see, => better performance
         data={games}
         keyExtractor={game => game.id}  
-        renderItem={({ item }) => <GameCard game={item} />} 
+        renderItem={({ item, index }) => 
+        <AnimatedGameCard game={item} index={index} />} 
     />
       )}
 </View>
