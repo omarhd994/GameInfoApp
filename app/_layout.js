960b6ffb0ed665ca1257a2b49998d2be2e4972c7
omarhd994 <wildsniper94@gmail.com>
2024-12-28 12:20:00 +0100

    expo router added

---
+++ app/_layout.js
@@ -0,0 +1,27 @@
+import { Slot } from "expo-router";
+import { View, Text } from "react-native";
+
+
+export default function Layout() {
+
+  return(
+    <View style={Style.Container}>
+      <Slot/>
+    </View>
+  )
+};
+
+
+const Style = {
+  Container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Title: {
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,7 @@
+import { Main } from "../component/Main";
+
+export default function App() { 
+    return (
+        <Main/>
+    );
+}
