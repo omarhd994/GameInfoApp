06f2743c0de0dcb1716838822a1bee6e5fbfe613
omarhd994 <wildsniper94@gmail.com>
2025-01-02 20:05:37 +0100

    short_description fixed, thumbnail, title .. yet to add

--- app/[GameDescription].js
+++ app/[GameDescription].js
@@ -1,30 +1,44 @@
-import {Text, View} from 'react-native';
-import { Link } from 'expo-router';
+import { ActivityIndicator, ScrollView, Text, View } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { Screen } from '../component/Screen'; 
 import { Stack } from 'expo-router';
+import { useState, useEffect } from 'react';
+import { getLatestGames } from '../lib/metacritic';
 
 export default function Detail() {
     const { GameDescription } = useLocalSearchParams();
+    const [ gameInfo, setGameInfo ] = useState("");
+
+  
+      useEffect(() => {
+        if (GameDescription) 
+          setGameInfo(GameDescription);
+        
+      }, [GameDescription]);
+
     return (
-    <Screen>
-      <Stack.Screen 
-        options={{
-           headerStyle: { backgroundColor: '#ffee00' },
-           headerTintColor: 'black',
-           headerLeft: () => {},
-           headerTitle: 'The Legend of Zelda: Breath of the Wild',
-           headerRight: () => {},
-        }}
-      />
-
-        <View>
-            <Text  style={{color: 'white', marginBottom: 20,}}> Detail Page {GameDescription}</Text>
-            <Link href='/' style={{color: 'blue'}}>
-            Go Back!
-            </Link>
-        </View>
-
-    </Screen>
-    );
-}
+      <Screen>
+        <Stack.Screen 
+          options={{
+             headerStyle: { backgroundColor: '#ffee00' },
+             headerTintColor: 'black',
+             headerLeft: () => {},
+             headerTitle: "Zelda",
+             headerRight: () => {},
+          }}
+        />
+          <View>
+            {GameDescription === null ? (<ActivityIndicator color={'red'} size={'large'}/>) 
+            : ( 
+          <ScrollView>
+            <View>
+              <Text  style={{ flex: 1, color: 'white', marginBottom: 20, alignContent: 'center', justifyContent: 'center'}}> 
+                Detail Page {gameInfo}</Text>
+            </View>
+          </ScrollView>
+              )}
+         </View>
+  
+      </Screen>
+      );
+  }
